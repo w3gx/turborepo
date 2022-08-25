12e058506f5b3cc56a048894a4a7eef07897733e
elcharitas <jonathanirhodia@gmail.com>
2022-08-25 12:45:18 +0100

    chore: prefix packages in repo

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+import { Button } from "@w3gx/ui";
 
 export default function Docs() {
-  return (
-    <div>
-      <h1>Docs</h1>
-      <Button />
-    </div>
-  );
+    return (
+        <div>
+            <h1>Docs</h1>
+            <Button variant="contained">Hello</Button>
+        </div>
+    );
 }
